79a0e2b516942cfca51d9825037127edd71ef022
ChenWei <bamboo80323@yahoo.com.tw>
2018-07-27 00:07:47 +0800

    Convert to MVC design pattern.

---
+++ PracticeTodoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  PracticeTodoey
+//
+//  Created by ChenWei on 2018/7/26.
+//  Copyright © 2018年 ChenWei. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+ 
